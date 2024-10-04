8a8878e6d73467c52933c549335f97767197548c
Yang Wang <wangyang.felix@hotmail.com>
2024-10-04 12:53:33 +0200

    add header file

---
+++ minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yawang <yawang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 12:25:40 by yawang            #+#    #+#             */
+/*   Updated: 2024/10/04 12:51:57 by yawang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+# include "ft_printf/libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+#endif
